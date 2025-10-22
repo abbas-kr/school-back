fe2b4be26c3ebf87f4c9a0232a3392a8857fc34a
Abbas <Abbas.kargar.1998@gmail.com>
2025-10-22 23:39:06 +0330

    move route + add logout route

--- routes/api.php
+++ routes/api.php
@@ -13,27 +13,3 @@ Route::post('/register', function (Request $request) {
     ]);
 });
 
-Route::middleware("web")->post('/login', function (Request $request) {
-    $credentials = $request->validate([
-        'username' => ['required'],
-        'password' => ['required'],
-    ]);
-
-    if (Auth::attempt($credentials)) {
-        $user = Auth::user();
-        $request->session()->regenerate();
-
-        return response()->json([
-            'success' => true,
-            'user' => $user
-        ]);
-    }
-
-    return response()->json(['success'=> false, 'message' => 'نام کاربری یا رمز عبور اشتباه است']);
-})->name('login');
-
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
